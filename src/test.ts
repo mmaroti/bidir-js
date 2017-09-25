5499eafdd369a242b45bcf64640cffe7f3cb5871
Miklos Maroti <mmaroti@gmail.com>
2017-09-25 18:00:56 -0500

    just playing around

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,18 @@
-function test(value: string) {
+class OpNode {
+    value: any;
+
+    filter(prefix: string) {
+        return new Filter(this, prefix);
+    }
 }
 
-test("hello");
-// test(1234);
+class Filter extends Node {
+    parent: OpNode;
+    prefix: string;
+
+    constructor(parent: OpNode, prefix: string) {
+        super();
+        this.parent = parent;
+        this.prefix = prefix;
+    }
+}
