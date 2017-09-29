ff74dbd0d104af3fb1fc0ba7e65d279a8d7dd36a
Miklos Maroti <mmaroti@gmail.com>
2017-09-29 13:53:07 -0500

    we are almost done

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,38 @@
+class Value {
+    value: any;
+
+    constructor(value: any) {
+        this.value = value;
+    }
+
+    filter(prefix: string): Value {
+        if (typeof this.value === "object") {
+            var r: any = {}
+            for (var x in this.value) {
+                if (x.startsWith(prefix)) {
+                    var y = x.substring(prefix.length);
+                    r[y] = this.value[x];
+                }
+            }
+            return new Value(r);
+        }
+        else {
+            return new Value(null);
+        }
+    }
+};
+
+function test(something: any) {
+    return something.filter("he");
+}
+
+var a = new Value({ hello: 12 });
+console.log(a);
+console.log(test(a));
+
+var b = new Value(["one", "two", "three", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+console.log(b.filter("1"));
+
 class OpNode {
     value: any;
 
@@ -6,7 +41,7 @@ class OpNode {
     }
 }
 
-class Filter extends Node {
+class Filter extends OpNode {
     parent: OpNode;
     prefix: string;
 
